6b672f27030d43ee0e5a0a305d88b74914f7752b
jwKim <jwKim@jwKim-PC>
2016-11-15 01:50:32 +0900

    react Bootstrap 추가

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,41 @@
 var React = require('react');
 var ReactRouter = require('react-router');
+var Navbar = require('react-bootstrap/lib/Navbar');
+var Nav = require('react-bootstrap/lib/Nav');
+var NavItem = require('react-bootstrap/lib/NavItem');
+var NavDropdown = require('react-bootstrap/lib/NavDropdown');
+var MenuItem = require('react-bootstrap/lib/MenuItem');
 var Link = ReactRouter.Link;
 
 var Home = React.createClass({		  
 	render: function(){
 		return(
 			<div>
+				<Navbar inverse collapseOnSelect>
+					<Navbar.Header>
+				    <Navbar.Brand>
+				        <a href="#">React-Bootstrap</a>
+				      </Navbar.Brand>
+				      <Navbar.Toggle />
+				    </Navbar.Header>
+				    <Navbar.Collapse>
+				      <Nav>
+				        <NavItem eventKey={1} href="#">Link</NavItem>
+				        <NavItem eventKey={2} href="#">Link</NavItem>
+				        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+				          <MenuItem eventKey={3.1}>Action</MenuItem>
+				          <MenuItem eventKey={3.2}>Another action</MenuItem>
+				          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+				          <MenuItem divider />
+				          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+				        </NavDropdown>
+				      </Nav>
+				      <Nav pullRight>
+				        <NavItem eventKey={1} href="#">Link Right</NavItem>
+				        <NavItem eventKey={2} href="#">Link Right</NavItem>
+				      </Nav>
+				    </Navbar.Collapse>
+			    </Navbar>
 				<header id="header">
 					<h1><Link to="/">header</Link></h1>
 					<div>
