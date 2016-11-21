953a6f287f9145f9a8d5481951c5321b4bfe537e
jwKim <jwKim@jwKim-PC>
2016-11-22 01:18:49 +0900

    수정

--- src/components/reactBoard/ReactBoard.jsx
+++ src/components/reactBoard/ReactBoard.jsx
@@ -20,6 +20,58 @@ var products = [{
     id: 4,
     name: "Product4",
     price: 80
+}, {
+    id: 5,
+    name: "Product5",
+    price: 80
+}, {
+    id: 6,
+    name: "Product6",
+    price: 80
+}, {
+    id: 7,
+    name: "Product7",
+    price: 80
+}, {
+    id: 8,
+    name: "Product8",
+    price: 80
+}, {
+    id: 9,
+    name: "Product9",
+    price: 80
+}, {
+    id: 10,
+    name: "Product10",
+    price: 80
+}, {
+    id: 11,
+    name: "Product11",
+    price: 80
+}, {
+    id: 12,
+    name: "Product12",
+    price: 80
+}, {
+    id: 13,
+    name: "Product13",
+    price: 80
+}, {
+    id: 14,
+    name: "Product14",
+    price: 80
+}, {
+    id: 15,
+    name: "Product15",
+    price: 80
+}, {
+    id: 16,
+    name: "Product16",
+    price: 80
+}, {
+    id: 17,
+    name: "Product17",
+    price: 80
 }];
 
 var ReactBoard = React.createClass({
@@ -27,7 +79,7 @@ var ReactBoard = React.createClass({
 	render : function(){
 		return(
 				<div>
-					<BootstrapTable data={ products } height='120'>
+					<BootstrapTable data={ products } height='700'>
 			        	<TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
 			        	<TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
 			        	<TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
