159a74bd50e58ea52c5c2c229ea69e2f6fc2b6f1
jwKim <jwKim@jwKim-PC>
2017-03-01 13:36:05 +0900

    버튼 반영

--- src/components/jwSpace/JwInvitation.jsx
+++ src/components/jwSpace/JwInvitation.jsx
@@ -3,8 +3,6 @@ var Image = require('react-bootstrap/lib/Image');
 
 var invitationAImg =  require('../../assets/img/초대-A.jpg');
 var invitationBImg =  require('../../assets/img/초대-B.jpg');
-var invitationDadImg =  require('../../assets/img/초대-B(아빠).jpg');
-var invitationMomImg =  require('../../assets/img/초대-B(엄마).jpg');
 var invitationDImg =  require('../../assets/img/초대-D.jpg');
 
 var JwInvitation = React.createClass({
@@ -19,10 +17,8 @@ var JwInvitation = React.createClass({
 						<Image className="invitationBImg" src={invitationBImg}></Image>
 					</div>	
 					<div  className="invitationB2">
-						<a href="sms:010-8417-4005">
-							<Image className="invitationDadImg" src={invitationDadImg}></Image>
-						</a>
-						<Image className="invitationMomImg" src={invitationMomImg}></Image>
+						<a href="sms:010-8417-4005" ><div className="invitationDadImg"></div></a>
+						<a href="sms:010-5619-0717" ><div className="invitationMomImg"></div></a>
 					</div>
 				</div>
 				<div className="invitationD">
